a45c2f82e34f499dd01f33d9db21494e82f8aeee
Tiberius Mairura <tiberiusmairura90@gmail.com>
2021-03-09 18:04:06 +0300

    fix

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const menuLinks = Array.from(document.querySelectorAll('.menu-link'));
+
+// menuLinks.forEach((link, index) => {
+//   link.addEventListener('click', (e) => {
+//     if (e.target.classList.contains('active')) {
+//       return;
+//     } else {
+//       link.classList.add('active');
+//       link[index + 1].classList.remove('active') ||
+//         link[index - 1].classList.remove('active');
+//     }
+//   });
+// });
