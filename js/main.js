034cc2628689ab7d39f401b5cff0b5528e1ae55c
Tiberius Mairura <tiberiusmairura90@gmail.com>
2021-03-09 18:30:53 +0300

    change colors

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 const menuLinks = Array.from(document.querySelectorAll('.menu-link'));
 
-// menuLinks.forEach((link, index) => {
-//   link.addEventListener('click', (e) => {
-//     if (e.target.classList.contains('active')) {
-//       return;
-//     } else {
-//       link.classList.add('active');
-//       link[index + 1].classList.remove('active') ||
-//         link[index - 1].classList.remove('active');
-//     }
-//   });
-// });
+menuLinks.forEach((link, index) => {
+  link.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) {
+      return;
+    } else {
+      link.classList.add('active');
+      if (link[index + 1].classList.remove('active')) {
+        link[index + 1].classList.add('active');
+      } else {
+        link[index - 1].classList.remove('active');
+      }
+    }
+  });
+});
