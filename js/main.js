40f13f9f6a9fe0c7a0ad4e875d18aabab2d635a7
Tiberius Mairura <tiberiusmairura90@gmail.com>
2021-03-09 19:45:31 +0300

    fix acitve class toggle

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 const menuLinks = Array.from(document.querySelectorAll('.menu-link'));
 
-menuLinks.forEach((link, index) => {
+const makeLinkActive = (link) => {
+  menuLinks.forEach((menuLink) => menuLink.classList.remove('active'));
+  link.classList.add('active');
+};
+
+menuLinks.forEach((link) => {
   link.addEventListener('click', (e) => {
     if (e.target.classList.contains('active')) {
       return;
     } else {
-      link.classList.add('active');
-      if (link[index + 1].classList.remove('active')) {
-        link[index + 1].classList.add('active');
-      } else {
-        link[index - 1].classList.remove('active');
-      }
+      makeLinkActive(e.target);
     }
   });
 });
